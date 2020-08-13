3febc74e9fa4ea0599f3fea0cb0af1455762fd72
Paulo Henrique <paulohenrique@MacBook-Air-de-Paulo.local>
2020-08-13 13:37:35 -0300

    add Order entity, mapped table relations

--- src/main/java/com/phdev/springwebservice/entities/User.java
+++ src/main/java/com/phdev/springwebservice/entities/User.java
@@ -1,14 +1,27 @@
 package com.phdev.springwebservice.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.phdev.springwebservice.repositories.Order;
+
 
 @Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
 
 	private static final long serialVersionUID = 1L;
 	@Id
@@ -18,6 +31,9 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	public User() {
 	}
@@ -68,7 +84,11 @@ public class User implements Serializable {
 	}
 
 	public void setPassword(String password) {
-		this.password = password;
+
+	}
+	
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 	@Override
