234fdb20bb4b809a8ec1eefe8fc5eb853c101a42
Paulo Henrique <paulohenrique@MacBook-Air-de-Paulo.local>
2020-08-13 22:29:08 -0300

    add Product entity, endoint and layers

---
+++ src/main/java/com/phdev/springwebservice/service/ProductService.java
@@ -0,0 +1,29 @@
+package com.phdev.springwebservice.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.phdev.springwebservice.entities.Product;
+import com.phdev.springwebservice.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll(){
+		return repository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		
+		Optional<Product> obj = repository.findById(id);
+		
+		return obj.get();
+	}
+	
+
+}
